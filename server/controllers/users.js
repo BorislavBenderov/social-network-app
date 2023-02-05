b982ac0a47c43453808d6639a4891dfe4535a064
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-05 11:32:23 +0200

    Add users controller and updateUser option

---
+++ server/controllers/users.js
@@ -0,0 +1,26 @@
+import bcrypt from "bcrypt";
+import User from "../models/User.js";
+
+export const updateUser = async (req, res) => {
+  if (req.body.userId === req.params.id || req.user.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (error) {
+        return res.status(500).json(error);
+      }
+    }
+
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Account has been updated");
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  } else {
+    return res.status(403).json("You can update only your account");
+  }
+};
