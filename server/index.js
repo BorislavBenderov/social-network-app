043e7cb89f0ece44a656e05e004d7556f18b8e0a
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-05 09:49:28 +0200

    Connect MongoDB

--- server/index.js
+++ server/index.js
@@ -7,6 +7,11 @@ import morgan from "morgan";
 const app = express();
 dotenv.config();
 
+mongoose.set("strictQuery", false);
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
+  console.log("Connected to MongoDB");
+});
 
 app.listen(8800, () => {
   console.log("Server listening!");
