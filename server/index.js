c264d6d59c6b9d1cb0e72fefba3b0a7c1a900fa1
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-05 10:42:58 +0200

    Use authRoute and userRoute

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
+import userRoute from "./routes/users.js";
+import authRoute from "./routes/auth.js";
 
 const app = express();
 dotenv.config();
@@ -17,6 +19,9 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
 app.use(json());
 app.use(helmet());
 app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
   console.log("Server listening!");
