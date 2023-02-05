c911211130b8ba13673f584208bc7ab81d2aa12c
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-05 10:41:54 +0200

    Use helmet and morgan

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { json } from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import helmet from "helmet";
@@ -12,6 +12,11 @@ mongoose.set("strictQuery", false);
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
   console.log("Connected to MongoDB");
 });
+
+//middleware
+app.use(json());
+app.use(helmet());
+app.use(morgan("common"));
 
 app.listen(8800, () => {
   console.log("Server listening!");
