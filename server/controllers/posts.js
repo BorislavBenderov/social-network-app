606a67f3f41b10d479a95635638d2f73b35e154c
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-06 08:52:48 +0200

    Add deletePost

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -23,3 +23,17 @@ export const updatePost = async (req, res) => {
     res.status(500).json(error);
   }
 };
+
+export const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+      res.status(200).json("The post has been deleted!");
+    } else {
+      res.status(403).json("You can delete only your posts!");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
