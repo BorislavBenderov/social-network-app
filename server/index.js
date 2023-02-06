357cd3062203988c892214126544cdb2375e09a9
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-06 08:39:40 +0200

    Use postRoute

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import userRoute from "./routes/users.js";
 import authRoute from "./routes/auth.js";
+import postRoute from "./routes/posts.js";
 
 const app = express();
 dotenv.config();
@@ -20,8 +21,9 @@ app.use(json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 
 app.listen(8800, () => {
   console.log("Server listening!");
