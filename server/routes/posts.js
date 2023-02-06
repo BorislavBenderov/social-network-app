607779ec89d1a2d13144b973dfd239fe0718f898
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-06 08:51:10 +0200

    Use updatePost

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createPost } from "../controllers/posts";
+import { createPost, updatePost } from "../controllers/posts";
 
 const router = express.Router();
 
 router.post("/", createPost);
+router.put("/:id", updatePost);
 
 export default router;
