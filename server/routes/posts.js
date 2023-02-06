dab6d72a386963186eba50c6767df4bb71004224
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-06 09:03:13 +0200

    Use likePost

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,16 @@
 import express from "express";
-import { createPost, deletePost, updatePost } from "../controllers/posts";
+import {
+  createPost,
+  deletePost,
+  likePost,
+  updatePost,
+} from "../controllers/posts";
 
 const router = express.Router();
 
 router.post("/", createPost);
 router.put("/:id", updatePost);
 router.delete("/:id", deletePost);
+router.put("/:id/like", likePost);
 
 export default router;
