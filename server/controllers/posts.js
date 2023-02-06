8eb048be9d9fcdced44462004abd709fa0a96e4e
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-06 08:50:58 +0200

    Add updatePost

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -9,3 +9,17 @@ export const createPost = async (req, res) => {
     res.status(500).json(error);
   }
 };
+
+export const updatePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+      res.status(200).json("The post has been updated!");
+    } else {
+      res.status(403).json("You can update only your posts!");
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
