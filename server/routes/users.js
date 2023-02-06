f2e98d4ba330851136f721efd43abf83fbb35d0e
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-06 08:34:08 +0200

    Use unfollowUser

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { deleteUser, followUser, getUser, updateUser } from "../controllers/users.js";
+import {
+  deleteUser,
+  followUser,
+  getUser,
+  unfollowUser,
+  updateUser,
+} from "../controllers/users.js";
 
 const router = express.Router();
 
@@ -7,5 +13,6 @@ router.get("/:id", getUser);
 router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
 router.put("/:id/follow", followUser);
+router.put("/:id/unfollow", unfollowUser);
 
 export default router;
