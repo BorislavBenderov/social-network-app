7728405bc7a15f88999184b8f995d8c4ae22ef37
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-07 14:43:52 +0200

    Add Online jsx

---
+++ client/src/components/online/Online.jsx
@@ -0,0 +1,13 @@
+import "./Online.scss";
+
+export const Online = () => {
+  return (
+    <li className="rightbar__friend">
+      <div className="rightbar__profile__img__container">
+        <img className="rightbar__profile__img" src="" alt="" />
+        <span className="rightbar__online"></span>
+      </div>
+      <span className="rightbar__username">John Molly</span>
+    </li>
+  );
+};
