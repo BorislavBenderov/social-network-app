70bf0dea5bbc762a35a3934f6a9b1e35cd279a54
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-08 13:04:49 +0200

    Add possibility users to login

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,6 +1,32 @@
+import { useContext, useState } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import { Link, useNavigate } from "react-router-dom";
+import { login } from "../../api";
 import "./Login.scss";
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { userLogin } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const onLogin = (e) => {
+    e.preventDefault();
+
+    const authData = {
+      email,
+      password,
+    };
+
+    login(authData)
+      .then((req) => {
+        userLogin(req.data);
+        navigate("/");
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
   return (
     <div className="login">
       <div className="login__wrapper">
@@ -12,12 +38,25 @@ export const Login = () => {
         </div>
         <div className="login__right">
           <div className="login__box">
-            <input className="login__input" placeholder="Email" />
-            <input className="login__input" placeholder="Password" />
-            <button className="login__button">Log In</button>
+            <input
+              className="login__input"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              className="login__input"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={onLogin} className="login__button">
+              Log In
+            </button>
             <span className="login__forgot">Forgot Password?</span>
             <button className="login__register__button">
-              Create a New Account
+              <Link to="/register">Create a New Account</Link>
             </button>
           </div>
         </div>
