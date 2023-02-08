ac0f367141ecfb4ee08b7d3d119ec4584f6baf62
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-08 13:09:56 +0200

    Export components

---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+export { Feed } from "./feed/Feed";
+export { Rightbar } from "./rightbar/Rightbar";
+export { Sidebar } from "./sidebar/Sidebar";
+export { Topbar } from "./topbar/Topbar";
+export { Share } from "./share/Share";
+export { Post } from "./post/Post";
