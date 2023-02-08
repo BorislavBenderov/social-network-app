2d31e600e4807b08646369d2c39af33015ce0c2f
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-08 13:09:03 +0200

    Use getAllUserPosts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -2,11 +2,12 @@ import express from "express";
 import {
   createPost,
   deletePost,
+  getAllUserPosts,
   getPost,
   likePost,
   timelinePosts,
   updatePost,
-} from "../controllers/posts";
+} from "../controllers/posts.js";
 
 const router = express.Router();
 
@@ -15,6 +16,7 @@ router.post("/", createPost);
 router.put("/:id", updatePost);
 router.delete("/:id", deletePost);
 router.put("/:id/like", likePost);
-router.get("/timeline/all", timelinePosts);
+router.get("/timeline/:userId", timelinePosts);
+router.get("/profile/:username", getAllUserPosts);
 
 export default router;
