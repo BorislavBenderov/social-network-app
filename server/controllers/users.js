502fbe94eebf852410a6cf49b71dd082e7483eb1
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-09 14:50:30 +0200

    Add getFriends in users controller

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -2,13 +2,37 @@ import bcrypt from "bcrypt";
 import User from "../models/User.js";
 
 export const getUser = async (req, res) => {
-  try {
-    const user = User.findById(req.params.id);
-    const { password, updatedAt, ...other } = user._doc;
+  const userId = req.query.userId;
+  const username = req.query.username;
 
+  try {
+    const user = userId
+      ? await User.findById(userId)
+      : await User.findOne({ username: username });
+    const { password, updatedAt, ...other } = user._doc;
     res.status(200).json(other);
-  } catch (error) {
-    res.status(500).json(error);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+export const getFriends = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    console.log(req.params);
+    const friends = await Promise.all(
+      user.followings.map((friendId) => {
+        return User.findById(friendId);
+      })
+    );
+    let friendList = [];
+    friends.map((friend) => {
+      const { _id, username, profilePicture } = friend;
+      friendList.push({ _id, username, profilePicture });
+    });
+    res.status(200).json(friendList);
+  } catch (err) {
+    res.status(500).json(err);
   }
 };
 
