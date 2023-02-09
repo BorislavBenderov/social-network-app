775e6a0465ed5a76659c4fb715c259c3acaa890d
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-09 14:47:06 +0200

    Fetch timelinePosts

--- client/src/contexts/PostContext.jsx
+++ client/src/contexts/PostContext.jsx
@@ -1,12 +1,25 @@
-import { createContext } from "react";
-
+import { createContext, useContext, useEffect, useState } from "react";
+import { getTimelinePosts } from "../api";
+import { AuthContext } from "./AuthContext";
 
 export const PostContext = createContext();
 
 export const PostContextProvider = ({ children }) => {
-  
+  const [timelinePosts, setTimelinePosts] = useState([]);
+  const { loggedUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    getTimelinePosts(loggedUser._id).then((res) => {
+      setTimelinePosts(
+        res.data.sort((p1, p2) => {
+          return new Date(p2.createdAt) - new Date(p1.createdAt);
+        })
+      );
+    });
+  }, []);
+
   return (
-    <PostContext.Provider value={{  }}>
+    <PostContext.Provider value={{ timelinePosts  }}>
       {children}
     </PostContext.Provider>
   );
