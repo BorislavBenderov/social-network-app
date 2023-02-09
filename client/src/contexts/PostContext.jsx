54f835991d8789af89bf2de3f61b3c9653375eee
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-09 14:49:02 +0200

    Update state when post is liked

--- client/src/contexts/PostContext.jsx
+++ client/src/contexts/PostContext.jsx
@@ -27,9 +27,31 @@ export const PostContextProvider = ({ children }) => {
     });
   };
 
+  const onLikePost = (userId, postId) => {
+    setTimelinePosts((state) => {
+      const post = state.find((x) => x._id === postId);
+      const likes = post.likes || [];
+
+      if (post.likes.includes(userId)) {
+        const i = post.likes.indexOf(userId);
+        likes.splice(i, 1);
+      } else {
+        likes.push(userId);
+      }
+
+      const newState = [
+        ...state.filter((x) => x._id !== postId),
+        { ...post, likes },
+      ];
+
+      return newState.sort((p1, p2) => {
+        return new Date(p2.createdAt) - new Date(p1.createdAt);
+      });
+    });
+  };
 
   return (
-    <PostContext.Provider value={{ timelinePosts onCreatePost }}>
+    <PostContext.Provider value={{ timelinePosts, onLikePost, onCreatePost }}>
       {children}
     </PostContext.Provider>
   );
