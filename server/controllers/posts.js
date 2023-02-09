642e8f52add73e9ea07cccbef67f1c57cdc8fda8
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-09 14:50:11 +0200

    Update post controller

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -66,17 +66,15 @@ export const likePost = async (req, res) => {
 export const timelinePosts = async (req, res) => {
   try {
     const currentUser = await User.findById(req.params.userId);
-
     const userPosts = await Post.find({ userId: currentUser._id });
-
     const friendPosts = await Promise.all(
       currentUser.followings.map((friendId) => {
         return Post.find({ userId: friendId });
       })
     );
-    res.json(userPosts.concat(...friendPosts));
-  } catch (error) {
-    res.status(500).json(error);
+    res.status(200).json(userPosts.concat(...friendPosts));
+  } catch (err) {
+    res.status(500).json(err);
   }
 };
 
