815ab8d619f0d57538bb17c8d6592b38ad2d3565
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-09 14:48:03 +0200

    Add possibility users to create posts

--- client/src/components/share/Share.jsx
+++ client/src/components/share/Share.jsx
@@ -3,8 +3,31 @@ import PermMediaIcon from "@mui/icons-material/PermMedia";
 import LabelIcon from "@mui/icons-material/Label";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import AddReactionIcon from "@mui/icons-material/AddReaction";
+import { createPost } from "../../api";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../contexts/AuthContext";
+import { PostContext } from "../../contexts/PostContext";
 
 export const Share = () => {
+  const [desc, setDesc] = useState("");
+  const { loggedUser } = useContext(AuthContext);
+  const { onCreatePost } = useContext(PostContext);
+
+  const onShare = (e) => {
+    e.preventDefault();
+
+    const data = {
+      userId: loggedUser._id,
+      username: loggedUser.username,
+      desc,
+    };
+
+    createPost(data).then((post) => {
+      onCreatePost(post.data);
+      setDesc("");
+    });
+  };
+
   return (
     <div className="share">
       <div className="share__wrapper">
@@ -14,7 +37,12 @@ export const Share = () => {
             src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
             alt=""
           />
-          <input placeholder="What's in your mind?" className="share__input" />
+          <input
+            placeholder="What's in your mind?"
+            className="share__input"
+            value={desc}
+            onChange={(e) => setDesc(e.target.value)}
+          />
         </div>
         <hr className="share__hr" />
         <div className="share__bottom">
@@ -36,7 +64,9 @@ export const Share = () => {
               <span className="share__option__text">Feelings</span>
             </div>
           </div>
-          <button className="share__button">Share</button>
+          <button onClick={onShare} className="share__button">
+            Share
+          </button>
         </div>
       </div>
     </div>
