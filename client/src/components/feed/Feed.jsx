26769536d14df7a110b5e0dcfea63d684e6aed46
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-10 21:36:32 +0200

    Render ProfileFeed and HomeFeed

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,18 +1,57 @@
 import { Post } from "../post/Post";
 import { Share } from "../share/Share";
 import "./Feed.scss";
-import { useContext  } from "react";
+import { useContext } from "react";
 import { PostContext } from "../../contexts/PostContext";
 
-export const Feed = () => {
-  const { timelinePosts } = useContext(PostContext);
+export const Feed = ({ profile }) => {
+  const { searchedTimelinePosts, searchedUserPosts, userPosts, loading } =
+    useContext(PostContext);
+
+  const HomeFeed = () => {
+    return (
+      <>
+        <Share />
+        {loading ? (
+          searchedTimelinePosts.length > 0 ? (
+            searchedTimelinePosts.map((post) => (
+              <Post key={post._id} post={post} />
+            ))
+          ) : (
+            <p className="no__searched__posts">
+              No posts found for the search criteria!
+            </p>
+          )
+        ) : (
+          <p className="loading">Loading...</p>
+        )}
+      </>
+    );
+  };
+
+  const ProfileFeed = () => {
+    return (
+      <>
+        <Share />
+        {loading ? (
+          userPosts.length > 0 ? (
+            searchedUserPosts.map((post) => <Post key={post._id} post={post} />)
+          ) : (
+            <p className="no__searched__posts">
+              No posts found for the search criteria!
+            </p>
+          )
+        ) : (
+          <p className="loading">Loading...</p>
+        )}
+      </>
+    );
+  };
+
   return (
     <div className="feed">
       <div className="feed__wrapper">
-        <Share />
-        {timelinePosts.map((post) => (
-          <Post key={post._id} post={post} />
-        ))}
+        {profile ? <ProfileFeed /> : <HomeFeed />}
       </div>
     </div>
   );
