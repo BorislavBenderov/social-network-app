c20ef1622c69b9edbdb081cc347b72c855a3617e
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-02-10 21:37:34 +0200

    Get user posts

--- client/src/contexts/PostContext.jsx
+++ client/src/contexts/PostContext.jsx
@@ -1,25 +1,71 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { getTimelinePosts } from "../api";
+import { getAllUserPosts, getTimelinePosts } from "../api";
 import { AuthContext } from "./AuthContext";
 
 export const PostContext = createContext();
 
 export const PostContextProvider = ({ children }) => {
+  const [searchTimelinePosts, setSearchTimelinePosts] = useState("");
+  const [searchUserPosts, setSearchUserPosts] = useState("");
   const [timelinePosts, setTimelinePosts] = useState([]);
+  const [userPosts, setUserPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
   const { loggedUser } = useContext(AuthContext);
 
   useEffect(() => {
-    getTimelinePosts(loggedUser._id).then((res) => {
-      setTimelinePosts(
-        res.data.sort((p1, p2) => {
-          return new Date(p2.createdAt) - new Date(p1.createdAt);
+    if (loggedUser.email) {
+      getTimelinePosts(loggedUser._id)
+        .then((res) => {
+          setTimelinePosts(
+            res.data.sort((p1, p2) => {
+              return new Date(p2.createdAt) - new Date(p1.createdAt);
+            })
+          );
+          setLoading(true);
         })
-      );
-    });
-  }, []);
+        .catch((err) => {
+          alert(err.message);
+        });
+    }
+  }, [searchTimelinePosts, loggedUser]);
+
+  useEffect(() => {
+    if (loggedUser.username) {
+      getAllUserPosts(loggedUser.username)
+        .then((res) => {
+          setUserPosts(
+            res.data.sort((p1, p2) => {
+              return new Date(p2.createdAt) - new Date(p1.createdAt);
+            })
+          );
+          setLoading(true);
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
+    }
+  }, [searchUserPosts, loggedUser]);
+
+  const searchedTimelinePosts = timelinePosts.filter(
+    (post) =>
+      post.desc?.toLowerCase().includes(searchTimelinePosts?.toLowerCase()) ||
+      post.username?.toLowerCase().includes(searchTimelinePosts?.toLowerCase())
+  );
+
+  const searchedUserPosts = userPosts.filter(
+    (post) =>
+      post.desc?.toLowerCase().includes(searchUserPosts?.toLowerCase()) ||
+      post.username?.toLowerCase().includes(searchUserPosts?.toLowerCase())
+  );
 
   const onCreatePost = (post) => {
     setTimelinePosts((oldState) => {
+      const newState = [...oldState, post];
+      return newState.sort((p1, p2) => {
+        return new Date(p2.createdAt) - new Date(p1.createdAt);
+      });
+    });
+    setUserPosts((oldState) => {
       const newState = [...oldState, post];
       return newState.sort((p1, p2) => {
         return new Date(p2.createdAt) - new Date(p1.createdAt);
@@ -51,7 +97,21 @@ export const PostContextProvider = ({ children }) => {
   };
 
   return (
-    <PostContext.Provider value={{ timelinePosts, onLikePost, onCreatePost }}>
+    <PostContext.Provider
+      value={{
+        timelinePosts,
+        userPosts,
+        searchedTimelinePosts,
+        searchedUserPosts,
+        onCreatePost,
+        onLikePost,
+        searchTimelinePosts,
+        setSearchTimelinePosts,
+        searchUserPosts,
+        setSearchUserPosts,
+        loading,
+      }}
+    >
       {children}
     </PostContext.Provider>
   );
